9f9d435ddd8680884e14867bafffd852c466ce77
Yeswanth Krishna <yeswanthkrishna55@gmail.com>
2026-06-15 15:06:39 +0530

    added home route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import {Signup} from './pages/Signup'
 import {Signin} from './pages/Signin'
 import {Blog} from './pages/Blog'
 import {Blogs} from './pages/Blogs'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import {BrowserRouter,Route,Routes,Navigate} from 'react-router-dom'
 import {Publish} from './pages/Publish'
 import './App.css'
 
@@ -12,6 +12,7 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path='/' element={<Navigate to='/blogs'></Navigate>}></Route>
           <Route path='/signup' element={<Signup />}/>
           <Route path='/signin' element={<Signin />}/>
           <Route path='/blog/:id' element={<Blog />}/>
